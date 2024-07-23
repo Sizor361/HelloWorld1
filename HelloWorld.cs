591ddab5e7281d4b8a079b4ba6585dbc6e24575d
Sizor361 <142895529+Sizor361@users.noreply.github.com>
2024-07-23 16:42:25 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    //В void Start пишутся команды, которые исполняются 1 раз при запуске игры
+    void Start()
+    {
+        print("Hello World!!!"); //Выводим в консоль какую-то информацию
+        print("Ещё что-нибудь"); //Выводим в консоль какую-то информацию
+    } 
+
+    void Update()
+    {
+        
+    }
+}
